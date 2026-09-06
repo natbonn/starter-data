c314025e6a9f3ffc37e046fd7defe648d548ea98
natbonn <natbon@hotmail.gr>
2026-09-06 16:19:05 +0300

    Initial commit

---
+++ src/main/java/gr/aueb/cf/starter_data/StarterDataApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.starter_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/aueb/cf/starter_data/StarterDataApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.starter_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
