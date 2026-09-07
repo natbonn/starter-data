15882fcfe16180cb13021b579b9d899635864664
natbonn <natbon@hotmail.gr>
2026-09-07 13:13:00 +0300

    Refactor TeacherRunner to demonstrate CRUD operations with Teacher entities

--- src/main/java/gr/aueb/cf/starter_data/runner/TeacherRunner.java
+++ src/main/java/gr/aueb/cf/starter_data/runner/TeacherRunner.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.UUID;
+
 @Component
 @RequiredArgsConstructor
 public class TeacherRunner implements CommandLineRunner {
@@ -21,22 +24,36 @@ public class TeacherRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Teacher teacher1 = new Teacher();
-        teacher1.setFirstname("John");
-        teacher1.setLastname("Papadopoulos");
-        teacher1.setActive(true);
-        teacherRepository.save(teacher1);        // κανει insert ή update αναλογα αν υπαρχει η οχι
+//        Teacher teacher1 = new Teacher();
+//        teacher1.setFirstname("John");
+//        teacher1.setLastname("Papadopoulos");
+//        teacher1.setActive(true);
+//        teacherRepository.save(teacher1);        // κανει insert ή update αναλογα αν υπαρχει η οχι
+//
+//        Teacher teacher2 = new Teacher();
+//        teacher2.setFirstname("Alice");
+//        teacher2.setLastname("Ioannou");
+//        teacher2.setActive(true);
+//        teacherRepository.save(teacher2);
+//
+//        Teacher teacher3 = new Teacher();
+//        teacher3.setFirstname("Bob");
+//        teacher3.setLastname("Giannoutsos");
+//        teacher3.setActive(false);
+//        teacherRepository.save(teacher3);
 
-        Teacher teacher2 = new Teacher();
-        teacher2.setFirstname("Alice");
-        teacher2.setLastname("Ioannou");
-        teacher2.setActive(true);
-        teacherRepository.save(teacher2);
+//        List<Teacher> teachers = teacherRepository.findAll();
+//        teachers.forEach(System.out::println);   // πρέπει να υπαρχει toString() στο Teacher
 
-        Teacher teacher3 = new Teacher();
-        teacher3.setFirstname("Bob");
-        teacher3.setLastname("Giannoutsos");
-        teacher3.setActive(false);
-        teacherRepository.save(teacher3);
+//        teacherRepository.findByUuid(UUID.fromString("84a03cd6-dd07-4b4a-bfcd-92e92ddb88b4"))
+//                .ifPresent(System.out::println);
+
+//        Crud με teacherRepository & επιπλέον queries ως ορίζουμε στο TeacherRepository
+//        Teacher teacher = teacherRepository.findById(2L)
+//                .orElseThrow(() -> new RuntimeException("Teacher not found"));
+//        teacher.setActive(false);                  // update του entity - το entity είναι managed
+//        teacherRepository.save(teacher);           // το hibernate θα κάνει update γιατί το entity είναι managed - dirty checking
+//        teacherRepository.delete(teacher);         // delete του entity - το entity είναι managed
+
     }
 }
